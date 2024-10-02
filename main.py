ad4c74ac87215b61248a69d3739586e389d5df45
João Vitor Sousa Ramos <79048652+Kaneliodas@users.noreply.github.com>
2024-10-02 19:22:46 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+my_foods = ['pizza', ' bolo', 'largatixa', ' queijo']
+friend_foods = my_foods
+my_foods.append("carttoni")
+print("Minhas comidas favoritas são :"+ str(my_foods))
+print("\nAs comidas favoritas do meu amigo são:"+ str(friend_foods))
